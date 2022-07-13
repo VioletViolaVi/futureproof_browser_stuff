9edbd7556c54a081953e8abf262a8c1638e4a1af
vivian <vivian.v18723@gmail.com>
2022-07-13 09:55:36 +0100

    add js practice

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("hello world");
+  // DO NOT USE innerHTML to add text
+  // USE .textContent INSTEAD
+  document.getElementById("mainTitle").textContent = "Hello World";
+
+  // using querySelector()
+  document.querySelector("#exampleSpan");
+
+  // using querySelectorAll() then looping
+  const allDiv = document.querySelectorAll("div");
+  for (const d of allDiv) {
+    d.innerText = "changed!";
+  }
+
+  // uses forEach()
+  allDiv.forEach((singleDiv, indexNum) => {
+    singleDiv.textContent = "These used forEach(): " + indexNum;
+  });
 });
